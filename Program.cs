30ab8835181c931140fe4902e42e54d89ff5ed96
AnhThuNe2022 <105047934+AnhThuNe2022@users.noreply.github.com>
2024-07-28 15:19:51 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebManagaTask.Data;
+using WebManagaTask.Models;
+//using WebManagaTask.Service.Interface;
+//using WebManagaTask.Service.Repository;
+using WebManagaTask.ViewModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
+//builder.Services.AddTransient<IEmailSenderInterface, EmailSenderRepository>();
+//builder.Services.AddScoped<EmployeeInterface, EmployeeRepository>();
+//builder.Services.AddScoped<OrderInterface, OrderRepository>();
+
+
+
+
+
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//        .AddEntityFrameworkStores<ApplicationDbContext>()
+//        .AddDefaultTokenProviders()
+//        .AddSignInManager<SignInManager<ApplicationUser>>();
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false; // Tắt xác thực email
+    options.SignIn.RequireConfirmedPhoneNumber = false; // Tắt xác thực số điện thoại
+})
+   .AddEntityFrameworkStores<ApplicationDbContext>()
+   .AddDefaultTokenProviders();
+
+builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    // Cookie settings
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.SlidingExpiration = true;
+});
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
+
+app.Run();
---
+++ ViewModels/ManageOrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebManagaTask.ViewModels
+{
+    public class ManageOrdersViewModel
+    {
+        public int EmployeeId { get; set; } 
+        public string? Name { get; set; }
+        public double? TotalMoney { get; set; }
+        public string? UpdateDate { get; set; }
+
+    }
+}
